c7891154858537a667da7642697bd47ff601b1a9
Fabrício Taveira <fabriciotaveira176@gmail.com>
2023-12-15 16:44:39 -0300

    Adjusting the algoritm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-
+const button = document.getElementById('buttonSort');
+const numberSortedHtml = document.getElementById('numberSorted');
+const numbersSorted = document.getElementById('numbersSorted');
+let i = 0;
+let numbersBingo = [];
+let j = 0;
 //shuffle the numbers
 function shuffle(array) {
-    for( let i = array.length -1; i > 0; i--){
+    for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
@@ -10,28 +15,48 @@ function shuffle(array) {
 
 
 //function to generate numbers
-function generateNumbers(){
+function generateNumbers() {
     const numbers = [];
-    for (let i = 1; i <= 200; i++){
+    for (let i = 1; i <= 75; i++) {
         numbers.push(i);
     }
     return shuffle(numbers);
 }
 
 //function to sort the numbers
-function sortNumbers(numbers){
-    if (numbers.length === 0 ){
-        return console.log('Todos os números foram sorteados!');
+function sortNumbers() {
+    if (i >= numbersBingo.length) {
+        return null
     }
 
-    const numberSorted = numbers.pop();
+    const numberSorted = numbersBingo[i];
     return numberSorted;
 }
 
-const numbersBingo = generateNumbers();
 
+button.addEventListener('click', () => {
+    if (i === 0) {
+        numbersBingo = generateNumbers();
+        shuffle(numbersBingo);
+    }
 
+    const numberSorted = sortNumbers();
 
-const numberSorted = sortNumbers(numbersBingo);
-console.log('O número sorteado foi ' + numberSorted);
+    if(numberSorted === null){
+        numbersSorted.innerHTML = 'Todos os números já foram sorteados';
+        return;
+    }
+
+    numberSortedHtml.innerHTML = numberSorted;
+    numbersSorted.innerHTML += numberSorted + ', ';
+
+    i += 1;
+    
+    j += 1;
+    if(j > 30){
+        numbersSorted.innerHTML = '';
+        j = 0;
+    }
+
+})
 
